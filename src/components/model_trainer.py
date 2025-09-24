605eac608d5c511557497edd9c64140e69fa248f
vidyanshu kushawaha <vidyanshukushawaha@gmail.com>
2025-09-24 20:54:51 +0530

    Hyperparameter Tuning Completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,14 +42,56 @@ class ModelTrainer:
                         models={
                                 "Random Forest":RandomForestRegressor(),
                                 "Decision Tree":DecisionTreeRegressor(),
-                                "Graidient Boosting":GradientBoostingRegressor(),
-                                "Linear Regression ": LinearRegression(),
-                                "K-Neighbour Regressor": KNeighborsRegressor(),
-                                "XGB Regressor":XGBRegressor(),
-                                "CatBoost regressor":CatBoostRegressor(verbose=False),
-                                "Ada Bosst Regressor":AdaBoostRegressor()
+                                "Linear Regression": LinearRegression(),
+                                # "K-Neighbour Regressor": KNeighborsRegressor(),
+                                "XGBRegressor":XGBRegressor(),
+                                "CatBoosting Regressor":CatBoostRegressor(verbose=False),
+                                "AdaBoost Regressor":AdaBoostRegressor(),
+                                # "Gradient Boosting":GradientBoostingRegressor()
                         }
-                        model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+                        logging.info("Model Listing compleated")
+                        params={
+
+                                "Decision Tree": {
+                                        'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                                        'splitter':['best','random'],
+                                        'max_features':['sqrt','log2'],
+                                 },
+                                "Random Forest":{
+                                        # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                                        # 'max_features':['sqrt','log2',None],
+                                        'n_estimators': [8,16,32,64,128,256]
+                                },
+                                "Linear Regression":{},
+                                "XGBRegressor":{
+                                        'learning_rate':[.1,.01,.05,.001],
+                                        'n_estimators': [8,16,32,64,128,256]
+                                },
+
+                                "CatBoosting Regressor":{
+                                        'depth': [6,8,10],
+                                        'learning_rate': [0.01, 0.05, 0.1],
+                                        'iterations': [30, 50, 100]
+                                },
+                                "AdaBoost Regressor":{
+                                        'learning_rate':[.1,.01,0.5,.001],
+                                         # 'loss':['linear','square','exponential'],
+                                        'n_estimators': [8,16,32,64,128,256]
+                                }
+                                # "Gradient Boosting":{
+                                #         'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                                #         'learning_rate':[.1,.01,.05,.001],
+                                #         'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                                #         'criterion':['squared_error', 'friedman_mse'],
+                                #         'max_features':['sqrt', 'log2', None],
+                                #         'n_estimators': [8,16,32,64,128,256]
+                                # }
+                
+                        }
+                        logging.info("Hyperparameter tuning params created")
+
+
+                        model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
                         best_model_score=max(sorted(model_report.values()))
 
@@ -65,7 +107,7 @@ class ModelTrainer:
                                 file_path=self.model_trainer_config.trained_model_file_path,
                                 obj=best_model
                         )
-                        # print("Best Model name :- ",best_model_name)
+                        logging.info(f"Best Model name :- {best_model_name}")
                         predicted=best_model.predict(X_test)
                         r2_scores=r2_score(y_test,predicted)
                         return r2_scores
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
+from src.logger import logging
 import numpy as np 
 import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-# from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,21 +23,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
+            logging.info(f"traning start for {model}")
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -45,7 +47,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
-
+            logging.info(f"R2 score of {model} is {test_model_score}")
             report[list(models.keys())[i]] = test_model_score
 
         return report
